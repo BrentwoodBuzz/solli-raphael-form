4d6bc2d70a5d042690835ed1fc40949e81b1db81
BrentwoodBuzz <brentwoodbuzz0@gmail.com>
2025-07-30 20:59:58 +1000

    _app.tsx made

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// src/pages/_app.tsx
+
+import '../index.css'; // Adjust the path if your CSS is elsewhere
+import { Contact } from '../components/Contact';
+
+
+export default function MyApp() {
+  return <Contact/> ;
+}
