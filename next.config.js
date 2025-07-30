4877ca3230b51925c76c52a0c0db00faa8c4dae9
BrentwoodBuzz <brentwoodbuzz0@gmail.com>
2025-07-30 20:39:38 +1000

    Es16 imports and js -> mjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/solli-raphael-form',
-    assetPrefix: '/solli-raphael-form',
-  }
-  
-  module.exports = nextConfig
-  
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/solli-raphael-form',
+  assetPrefix: '/solli-raphael-form',
+}
+
+export default nextConfig;
