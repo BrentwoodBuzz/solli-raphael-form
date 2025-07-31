94dea1d78a862fff29444000cd562269d8a6c413
BrentwoodBuzz <brentwoodbuzz0@gmail.com>
2025-07-31 20:32:06 +1000

    .ts extension rather than js cjs or mjs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const repo = 'solli-raphael-form'; // 🔁 Replace with your GitHub repo name
 
 module.exports = {
   output: 'export', // for static export
-  basePath: isGitHubPages ? `/${repo}` : '',
-  assetPrefix: isGitHubPages ? `/${repo}/` : '',
+  basePath: `/${repo}` ,
+  assetPrefix:  `/${repo}/`,
   trailingSlash: true,
 };
