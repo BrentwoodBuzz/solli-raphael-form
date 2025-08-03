b0d9d0990ce7c62b5e1619e26ae3f775b5462065
BrentwoodBuzz <brentwoodbuzz0@gmail.com>
2025-08-03 16:23:07 +1000

    yey

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 const repo = 'solli-raphael-form'; // change to your repo name
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isGitHubPages ? `/${repo}` : '',
-  assetPrefix: isGitHubPages ? `/${repo}/` : '',
+  basePath: '/solli-raphael-form',
+  assetPrefix: "/solli-raphael-form/",
   trailingSlash: true,
 };
 
