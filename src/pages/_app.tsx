292ed05216f9128649503caeef1c47e16bc3d1f5
BrentwoodBuzz <brentwoodbuzz0@gmail.com>
2025-08-07 17:48:10 +1000

    Tailwind input and output files to make css run

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 // src/pages/_app.tsx
-import '../index.css'; // Import your global CSS here
+import '../output.css'; // Import your global CSS here
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
